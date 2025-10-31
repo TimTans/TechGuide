4b87e25933c5b9c174c98535782de61529675aef
Timmer-1 <ttan001@citymail.cuny.edu>
2025-10-31 16:37:18 -0400

    added speed insights vercel

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,15 @@ import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
       <AuthContextProvider>
-        <RouterProvider router = {router}/>
+        <RouterProvider router={router} />
       </AuthContextProvider>
+      <SpeedInsights />
     </>
   </StrictMode>,
 )
