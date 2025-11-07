563b597e39a4f3315b40ffcfa06ebdb2033af9de
Misael Perez <misaelperez@mac.mynetworksettings.com>
2025-11-07 18:12:17 -0500

    adding visualizer for file and manualChunks for optimization

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import {visualizer} from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+  //Just adding another package to see what slows down in the system
+        visualizer({
+        filename: 'dist/stats.html',
+        title: 'Bundle Visualizer',
+        gzipSize: true,
+        brotliSize:true,
+        template:'sunburst',
+        open:true
+    }),],
+  build:{
+    sourcemap:false,
+    rollupOptions: {
+      output:{
+        manualChunks(id){
+          if (id.includes('node_modules')){
+            if (id.includes('@supabase')) return 'supabase-vendor'
+            if (id.includes('react')) return 'react-vendor'
+          }
+
+        },
+      },
+    },
+    
+  }
 })
